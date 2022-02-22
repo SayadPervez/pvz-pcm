978a6c775224392410c3369a5c84c341afbd0d52
Sayad Pervez <pervez2504@gmail.com>
2022-02-22 16:36:23 +0530

    reconstruction graph completed

--- js/reconWaveGraph.js
+++ js/reconWaveGraph.js
@@ -1,18 +1,21 @@
-let sourceWaveCanvas = document.getElementById('reconWaveCanvas');
-let sourceWaveCtx = sourceWaveCanvas.getContext('2d');
+let encodedWaveCanvas = document.getElementById('reconWaveCanvas');
+let encodedWaveCtx = encodedWaveCanvas.getContext('2d');
 
-let canvas_width = sourceWaveCanvas.parentElement.clientWidth || 1100;
+let canvas_width = encodedWaveCanvas.parentElement.clientWidth || 1100;
 let canvas_height = 250;
 let orgx = 50;
 let orgy = canvas_height / 2;
 
-sourceWaveCanvas.width = canvas_width;
-sourceWaveCanvas.height = canvas_height;
+encodedWaveCanvas.width = canvas_width;
+encodedWaveCanvas.height = canvas_height;
 
 const wave_amplitude_element = document.getElementById("swamplitude");
 const wave_frequency_element = document.getElementById("swfrequency");
+const sampling_frequency_element = document.getElementById("safrequency")
 const vertical_scale_element = document.getElementById("reconwave_vertical_scale_factor");
 const horizontal_scale_element = document.getElementById("reconwave_horizontal_scale_factor");
+const bl_scale_element = document.getElementById("bit_length_factor");
+const check_quantized_points = document.getElementById("quantized_points");
 
 // Draws the axes for the graph
 function drawAxes(ctx, orgx, orgy, line_start, line_end) {
@@ -42,6 +45,19 @@ function drawAxes(ctx, orgx, orgy, line_start, line_end) {
     ctx.closePath();
 }
 
+function drawPoint(ctx, x, y) {
+    var radius = 3.0;
+    ctx.beginPath();
+    ctx.strokeStyle = "blue";
+    ctx.stroke();
+    ctx.fillStyle = 'black';
+    ctx.lineWidth = 1;
+    ctx.arc(x, y, radius*1.3, 0, 2 * Math.PI, false);
+    ctx.fill();
+
+    ctx.closePath();
+}
+
 /*
  * Returns an array of values starting with value *start* ending
  * at value *stop* and with an increment of *step*.
@@ -57,85 +73,85 @@ function xrange(start, stop, step) {
     return res;
 }
 
+function d2b(x,bitLength=8)
+{
+    var result = "0000000000000000000000000"+(x >>> 0).toString(2);
+    return(result.substr(result.length-bitLength));
+}
+
 // Will draw the sine wave starting from loc xOffset, yOffset
-function plotSine(ctx, amplitude, frequency, xOffset, yOffset, vertical_scaling_factor, horizontal_scaling_factor) {
+function plotSine(ctx, amplitude, frequency, xOffset, yOffset, vertical_scaling_factor, horizontal_scaling_factor,mid_of_line) {
     var width = 1000;
-
-    // 1-5: 80
-    // 6-12: 140
-    // 13-20: 200
-    var Fs = 0;
-    if (frequency >= 1 && frequency <= 5) Fs = 80;
-    else if (frequency >= 6 && frequency <= 12) Fs = 140;
-    else Fs = 250;
-
-    // Generates the values for the sine wave.
+    var Fs = sampling_frequency.value;
     var StopTime = 1;
-    var dt = 1 / Fs;
-    var t = xrange(0, StopTime + dt, dt);
+    var dt = 1 / Fs; // sampling interval
+    var t = xrange(0, StopTime + dt, dt); // generates a list of t values seperated by sampling interval
+
     var x = [];
     t.forEach((val) => {
         x.push(amplitude * Math.sin(2 * Math.PI * frequency * val));
     });
+    //console.log('t size is ', t.length);
+    //console.log('x size is ', x.length);
 
 
     ctx.beginPath();
     ctx.lineWidth = 2;
     ctx.strokeStyle = "red";
-
-    // Draw the original sine wave.
-    var idx = 0;
-    while (idx < width && idx < x.length) {
-        ctx.lineTo(xOffset + idx * horizontal_scaling_factor, yOffset - vertical_scaling_factor * x[idx]);
-        idx++;
+    var q = [];
+    var delta = ((2 * Math.PI * amplitude * frequency) / Fs).toFixed(4);
+    if (true) {
+        var idx = 0;
+        while (idx < width) {
+            ctx.lineTo(xOffset + idx * horizontal_scaling_factor, yOffset - vertical_scaling_factor * (true ? Math.round(x[idx]) : x[idx]));
+            //q.push(Math.round(x[idx]));
+            //ctx.moveTo(xOffset + idx * horizontal_scaling_factor, yOffset - vertical_scaling_factor * (true ? Math.round(x[idx]) : x[idx]))
+            //ctx.lineTo(xOffset + idx * horizontal_scaling_factor, mid_of_line)
+            ctx.stroke();
+            idx++;
+        }
     }
-
-    // while (x.length < width) {
-    //     x = x.concat(x);
-    // }
-
-    // while (idx < width && idx < x.length) {
-    //     ctx.lineTo(xOffset + idx * horizontal_scaling_factor, yOffset - vertical_scaling_factor * x[idx]);
-    //     idx++;
-    // }
-
-    ctx.stroke();
     ctx.save();
-    ctx.font = "20px Arial";
-    ctx.fillStyle = "black";
-    ctx.fillText(`Amplitude: ${amplitude}v`, canvas_width - 200, 50, 110);
-    ctx.fillText(`Frequency: ${frequency}Hz`, canvas_width - 200, 80, 110);
-    ctx.closePath();
+    document.getElementById('binListSize').innerHTML = new Set(q).size;
+    //console.log(new Set(q));
 }
 
 let size_set = false;
 
-export function drawReconWave() {
-    const wave_amplitude = wave_amplitude_element.value;
-    const wave_frequency = wave_frequency_element.value;
-    const vertical_scaling_factor = vertical_scale_element.value;
-    const horizontal_scaling_factor = horizontal_scale_element.value;
+export function getQuantizationLevels() {
+    var Fs = sampling_frequency_element.value;
+    var StopTime = 1;
+    var dt = 1 / Fs; // sampling interval
+    var t = xrange(0, StopTime + dt, dt); // generates a list of t values seperated by sampling interval
+    return t.length;
+}
 
-    canvas_height = sourceWaveCanvas.parentElement.clientHeight;
-    canvas_width = sourceWaveCanvas.parentElement.clientWidth;
+export function drawReconWave() {
+    var wave_amplitude = wave_amplitude_element.value;
+    var wave_frequency = wave_frequency_element.value;
+    var vertical_scaling_factor = vertical_scale_element.value;
+    var horizontal_scaling_factor = horizontal_scale_element.value;
+
+    canvas_height = encodedWaveCanvas.parentElement.clientHeight;
+    canvas_width = encodedWaveCanvas.parentElement.clientWidth;
     if (canvas_height > 100 && !size_set) {
-        canvas_height = sourceWaveCanvas.parentElement.clientHeight;
-        canvas_width = sourceWaveCanvas.parentElement.clientWidth;
-        sourceWaveCtx.canvas.width = canvas_width;
-        sourceWaveCtx.canvas.height = canvas_height;
+        canvas_height = encodedWaveCanvas.parentElement.clientHeight;
+        canvas_width = encodedWaveCanvas.parentElement.clientWidth;
+        encodedWaveCtx.canvas.width = canvas_width;
+        encodedWaveCtx.canvas.height = canvas_height;
         size_set = true;
     }
 
     // Clear the screen
-    sourceWaveCtx.fillStyle = "white";
-    sourceWaveCtx.fillRect(0, 0, canvas_width, canvas_height);
+    encodedWaveCtx.fillStyle = "white";
+    encodedWaveCtx.fillRect(0, 0, canvas_width, canvas_height);
 
     // Vertical line start and end
-    const line_start = 20;
-    const line_end = canvas_height - 50;
-    const mid_of_line = (line_start + line_end) / 2;
+    var line_start = 20;
+    var line_end = canvas_height - 50;
+    var mid_of_line = (line_start + line_end) / 2;
 
-    drawAxes(sourceWaveCtx, orgx, orgy, line_start, line_end);
-    plotSine(sourceWaveCtx, wave_amplitude, wave_frequency, orgx, mid_of_line, vertical_scaling_factor, horizontal_scaling_factor);
+    drawAxes(encodedWaveCtx, orgx, orgy, line_start, line_end);
+    plotSine(encodedWaveCtx, wave_amplitude, wave_frequency, orgx, mid_of_line, vertical_scaling_factor, horizontal_scaling_factor,mid_of_line);
     requestAnimationFrame(drawReconWave);
 }
