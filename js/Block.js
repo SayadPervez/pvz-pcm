d1a9f1d1713a37bc1077f270d59b2a32b9cd830c
Sayad Pervez <pervez2504@gmail.com>
2022-02-25 21:29:33 +0530

    wires added

--- js/Block.js
+++ js/Block.js
@@ -1,3 +1,5 @@
+export let connectionNodes = [];
+
 class Block {
     constructor(x, y, w, h, name = null, onclick = null, selectable = false) {
         this.name = name;
@@ -70,6 +72,107 @@ class Block {
     doubleClickModal() { return null; }
 }
 
+function circle_dist(x1, y1, x2, y2) {
+    const xd = x2 - x1;
+    const yd = y2 - y1;
+    return xd * xd + yd * yd;
+}
+
+export class Wire {
+    constructor (components) {
+        this.components = components;
+        this.width = 2;
+        this.selected = false;
+    }
+
+    draw() {
+        push();
+        strokeWeight(this.width);
+        if (this.selected || this.didClick()) stroke(255, 0, 0);
+        else stroke(0, 0, 0);
+        
+        for (let i = 0; i < this.components.length - 1; i++) {
+            const start = this.components[i];
+            const end = this.components[i + 1];
+            line(start.x, start.y, end.x, end.y)
+        }
+
+        pop();
+    }
+
+    didClick() {
+        for (let i = 0; i < this.components.length - 1; i++) {
+            const start = this.components[i];
+            const end = this.components[i + 1];
+            const x1 = start.x, x2 = end.x;
+            const y1 = start.y, y2 = end.y;
+            let distance = [];
+
+            distance.push(dist(x1, y1,
+                mouseX, mouseY));
+            distance.push(dist(x2, y2,
+                mouseX, mouseY));
+            const wireLength = dist(x1, y1,
+                x2, y2);
+
+            if (distance[0] + distance[1] >= wireLength - (this.width / (10 * 2)) &&
+                distance[0] + distance[1] <= wireLength + (this.width / (10 * 2)))
+                return true;
+        }
+        return false;
+    }
+}
+
+export class WireManager {
+    constructor() {
+        this.wires = []
+    }
+
+    addWire(components) {
+        this.wires.push(new Wire(components));
+    }
+
+    draw() {
+        this.wires.forEach((wire) => {
+            wire.draw();
+        });
+    }
+
+    remove(wire) {
+        const idx = this.wires.indexOf(wire);
+        if (idx == -1) return ;
+        this.wires.splice(idx, 1);
+    }
+}
+
+export class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.r = 15;
+        this.selected = false;
+    }
+
+    didClick() {
+        const rdist = circle_dist(this.x, this.y, mouseX, mouseY)
+        return rdist < (this.r * this.r);
+    }
+
+    draw() {
+        push();
+        strokeWeight(4);
+        stroke(255, 0, 0);
+        if (this.selected || this.didClick()) {
+            fill(255, 255, 0);
+        } else {
+            fill(255, 255, 255);
+        }
+        circle(this.x, this.y, this.r);
+        pop();
+
+    }
+}
+
 export class SineGenerator extends Block {
     constructor (x, y, w, h) {
         super(x, y, w, h, 'SINE WAVE\nGENERATOR', null, true);
@@ -83,6 +186,8 @@ export class SineGenerator extends Block {
         };
         this.amplitude = this.amp_slider.value;
         this.frequency = this.freq_slider.value;
+        this.output = new Point(this.cx + this.cw + 7, this.cy + this.ch / 2);
+        connectionNodes.push(this.output);
     }
 
     populate_modal(modal) {
@@ -99,6 +204,10 @@ export class SineGenerator extends Block {
         modal.appendChild(this.freq_slider);
     }
 
+    draw() {
+        super.draw();
+        this.output.draw();
+    }
 
     doubleClickModal() {
         return 'sourceWaveGraph';
@@ -111,6 +220,10 @@ export class Sampler extends Block {
         this.sampling_frequency = 0;
         this.sampling_slider = createSlider(0, 5000, 500);
         this.sampling_slider.onchange = this.sampling_slider_change;
+        this.input = new Point(this.cx - 7, this.cy + this.ch / 2);
+        this.output = new Point(this.cx + this.cw + 7, this.cy + this.ch / 2);
+        connectionNodes.push(this.input);
+        connectionNodes.push(this.output);
     }
 
     sampling_slider_change() {
@@ -120,11 +233,27 @@ export class Sampler extends Block {
     doubleClickModal() {
         return 'sampledWaveGraph';
     }
+
+    draw() {
+        super.draw();
+        this.input.draw();
+        this.output.draw();
+    }
 }
 
 export class Quantizer extends Block {
     constructor (x, y, w, h) {
         super(x, y, w, h, 'QUANTIZER', null, true);
+        this.input = new Point(this.cx - 7, this.cy + this.ch / 2);
+        this.output = new Point(this.cx + this.cw + 7, this.cy + this.ch / 2);
+        connectionNodes.push(this.input);
+        connectionNodes.push(this.output);
+    }
+
+    draw() {
+        super.draw();
+        this.input.draw();
+        this.output.draw();
     }
 
     doubleClickModal() { return 'quantizerOutput'; }
@@ -133,35 +262,83 @@ export class Quantizer extends Block {
 export class LowPassFilter extends Block {
     constructor (x, y, w, h) {
         super(x, y, w, h, 'LOW PASS\nFILTER', null, true);
+        this.input = new Point(this.cx - 7, this.cy + this.ch / 2);
+        this.output = new Point(this.cx + this.cw + 7, this.cy + this.ch / 2);
+    }
+
+    draw() {
+        super.draw();
+        this.input.draw();
+        this.output.draw();
     }
 }
 
 export class PredictionFilter extends Block {
     constructor (x, y, w, h) {
         super(x, y, w, h, 'PREDICTION\nFILTER', null, true);
+        this.input = new Point(this.cx - 7, this.cy + this.ch / 2);
+        this.output = new Point(this.cx + this.cw + 7, this.cy + this.ch / 2);
+        connectionNodes.push(this.input);
+        connectionNodes.push(this.output);
+    }
+
+    draw() {
+        super.draw();
+        this.input.draw();
+        this.output.draw();
     }
 }
 
 export class ReconstructionFilter extends Block {
     constructor (x, y, w, h) {
         super(x, y, w, h, 'Reconstruction\nFILTER', null, true);
+        this.input = new Point(this.cx - 7, this.cy + this.ch / 2);
+        this.output = new Point(this.cx + this.cw + 7, this.cy + this.ch / 2);
+        connectionNodes.push(this.input);
+        connectionNodes.push(this.output);
     }
     doubleClickModal() { return 'reconWaveGraph'; }
+
+    draw() {
+        super.draw();
+        this.input.draw();
+        this.output.draw();
+    }
 }
 
 export class Encoder extends Block {
     constructor (x, y, w, h) {
         super(x, y, w, h, 'ENCODER', null, true);
+        this.input = new Point(this.cx - 7, this.cy + this.ch / 2);
+        this.output = new Point(this.cx + this.cw + 7, this.cy + this.ch / 2);
+        connectionNodes.push(this.input);
+        connectionNodes.push(this.output);
     }
 
     doubleClickModal() { return 'encodedWaveGraph'; }
+
+    draw() {
+        super.draw();
+        this.input.draw();
+        this.output.draw();
+    }
 }
 
 export class Decoder extends Block {
     constructor (x, y, w, h) {
         super(x, y, w, h, 'DECODER', null, true);
+        this.input = new Point(this.cx - 7, this.cy + this.ch / 2);
+        this.output = new Point(this.cx + this.cw + 7, this.cy + this.ch / 2);
+        connectionNodes.push(this.input);
+        connectionNodes.push(this.output);
     }
     doubleClickModal() {
         return 'decoderWaveGraph';
+    }
+
+    draw() {
+        super.draw();
+        this.input.draw();
+        this.output.draw();
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { SineGenerator, Sampler, Quantizer, ReconstructionFilter } from './Block.js'
-import { Encoder, Decoder } from './Block.js';
+import { Wire, connectionNodes, WireManager, Encoder, Decoder } from './Block.js';
 import { drawSourceWave } from './sourceWaveGraph.js';
 import { drawSampledWave } from './sampledWaveGraph.js';
 import { drawEncodedWave, getQuantizationLevels } from './encodedWaveGraph.js';
@@ -26,44 +26,44 @@ function setup_modulation() {
 
     /* FIXME: Find a new way to make the elements responsive to resize */
     
-    myblocks.set('line0', new Line((val) => {
-        const generator = myblocks.get('generator');
-        const sampler = myblocks.get('sampler');
+    // myblocks.set('line0', new Line((val) => {
+    //     const generator = myblocks.get('generator');
+    //     const sampler = myblocks.get('sampler');
 
-        val.x1 = generator.cx + generator.cw;
-        val.y1 = generator.cy + generator.ch / 2;
-        val.x2 = sampler.cx;
-        val.y2 = sampler.cy + sampler.ch / 2;
-    }, 0, 'x(t)'));
+    //     val.x1 = generator.cx + generator.cw;
+    //     val.y1 = generator.cy + generator.ch / 2;
+    //     val.x2 = sampler.cx;
+    //     val.y2 = sampler.cy + sampler.ch / 2;
+    // }, 0, 'x(t)'));
     
-    myblocks.set('line1', new Line((val) => {
-        const sampler = myblocks.get('sampler');
-        const quantizer = myblocks.get('quantizer');
+    // myblocks.set('line1', new Line((val) => {
+    //     const sampler = myblocks.get('sampler');
+    //     const quantizer = myblocks.get('quantizer');
 
-        val.x1 = sampler.cx + sampler.cw;
-        val.y1 = sampler.cy + sampler.ch / 2;
-        val.x2 = quantizer.cx;
-        val.y2 = quantizer.cy + quantizer.ch / 2;
-    }, 0, 'x(nTs)'));
+    //     val.x1 = sampler.cx + sampler.cw;
+    //     val.y1 = sampler.cy + sampler.ch / 2;
+    //     val.x2 = quantizer.cx;
+    //     val.y2 = quantizer.cy + quantizer.ch / 2;
+    // }, 0, 'x(nTs)'));
     
-    myblocks.set('line8', new Line((val) => {
-        const quantizer = myblocks.get('quantizer');
-        const encoder = myblocks.get('encoder');
+    // myblocks.set('line8', new Line((val) => {
+    //     const quantizer = myblocks.get('quantizer');
+    //     const encoder = myblocks.get('encoder');
 
-        val.x1 = quantizer.cx+quantizer.cw;
-        val.y1 = quantizer.cy + quantizer.ch / 2;
-        val.x2 = encoder.cx;
-        val.y2 = encoder.cy + encoder.ch / 2;
-    }, 0, 'xq(nTs)'));
+    //     val.x1 = quantizer.cx+quantizer.cw;
+    //     val.y1 = quantizer.cy + quantizer.ch / 2;
+    //     val.x2 = encoder.cx;
+    //     val.y2 = encoder.cy + encoder.ch / 2;
+    // }, 0, 'xq(nTs)'));
     
-    myblocks.set('line9', new Line((val) => {
-        const encoder = myblocks.get('encoder');
+    // myblocks.set('line9', new Line((val) => {
+    //     const encoder = myblocks.get('encoder');
 
-        val.x1 = encoder.cx+encoder.cw;
-        val.y1 = encoder.cy + encoder.ch / 2;
-        val.x2 = encoder.cx+encoder.cw+120;
-        val.y2 = encoder.cy + encoder.ch / 2;
-    }, 0 , "PCM\noutput"));
+    //     val.x1 = encoder.cx+encoder.cw;
+    //     val.y1 = encoder.cy + encoder.ch / 2;
+    //     val.x2 = encoder.cx+encoder.cw+120;
+    //     val.y2 = encoder.cy + encoder.ch / 2;
+    // }, 0 , "PCM\noutput"));
     
 }
 
@@ -71,30 +71,30 @@ function setup_demodulation() {
     myblocks.set('decoder', new Decoder(450+145, 587.6-170, 200, 100));
     myblocks.set('reconstructionfilter', new ReconstructionFilter(950+145, 587.6-170, 200, 100));
 
-    myblocks.set('line1dm', new Line((val) => {
-        const decoder = myblocks.get('decoder');
-        val.x1 = decoder.cx -220;
-        val.y1 = decoder.cy + (decoder.ch / 2);
-        val.x2 = decoder.cx;
-        val.y2 = decoder.cy + (decoder.ch / 2);
-    }, 0, 'Encoder Output'));
+    // myblocks.set('line1dm', new Line((val) => {
+    //     const decoder = myblocks.get('decoder');
+    //     val.x1 = decoder.cx -220;
+    //     val.y1 = decoder.cy + (decoder.ch / 2);
+    //     val.x2 = decoder.cx;
+    //     val.y2 = decoder.cy + (decoder.ch / 2);
+    // }, 0, 'Encoder Output'));
 
-    myblocks.set('line2dm', new Line((val) => {
-        const decoder = myblocks.get('decoder');
-        const reconstructionfilter = myblocks.get('reconstructionfilter');
-        val.x1 = decoder.cx + decoder.cw;
-        val.y1 = decoder.cy + (decoder.ch / 2);
-        val.x2 = reconstructionfilter.cx;
-        val.y2 = reconstructionfilter.cy + (reconstructionfilter.ch / 2);
-    }, 0, 'Decoded Output'));
+    // myblocks.set('line2dm', new Line((val) => {
+    //     const decoder = myblocks.get('decoder');
+    //     const reconstructionfilter = myblocks.get('reconstructionfilter');
+    //     val.x1 = decoder.cx + decoder.cw;
+    //     val.y1 = decoder.cy + (decoder.ch / 2);
+    //     val.x2 = reconstructionfilter.cx;
+    //     val.y2 = reconstructionfilter.cy + (reconstructionfilter.ch / 2);
+    // }, 0, 'Decoded Output'));
 
-    myblocks.set('line3dm', new Line((val) => {
-        const reconstructionfilter = myblocks.get('reconstructionfilter');
-        val.x1 = reconstructionfilter.cx + reconstructionfilter.cw;
-        val.y1 = reconstructionfilter.cy + (reconstructionfilter.ch / 2);
-        val.x2 = reconstructionfilter.cx + reconstructionfilter.cw + 220;
-        val.y2 = reconstructionfilter.cy + (reconstructionfilter.ch / 2);
-    }, 0, 'Reconstructed\nMessage Signal'));
+    // myblocks.set('line3dm', new Line((val) => {
+    //     const reconstructionfilter = myblocks.get('reconstructionfilter');
+    //     val.x1 = reconstructionfilter.cx + reconstructionfilter.cw;
+    //     val.y1 = reconstructionfilter.cy + (reconstructionfilter.ch / 2);
+    //     val.x2 = reconstructionfilter.cx + reconstructionfilter.cw + 220;
+    //     val.y2 = reconstructionfilter.cy + (reconstructionfilter.ch / 2);
+    // }, 0, 'Reconstructed\nMessage Signal'));
 
     /*
     myblocks.set('line2dm', new Line((val) => {
@@ -179,6 +179,68 @@ function doubleClicked() {
     });
 }
 
+let wireManager = new WireManager();
+let currentStartNode = null;
+let currentSelected = null;
+
+
+function keyPressed() {
+    if (keyCode === DELETE) {
+        if (currentSelected) {
+            console.log('removing ', currentSelected);
+            wireManager.remove(currentSelected);
+            currentSelected = null;
+        }
+        components = [];
+        if (currentStartNode) currentStartNode = null;
+    }
+    if (keyCode === ENTER) {
+        console.log(components);
+        console.log(wireManager);
+    }
+}
+
+let components = [];
+
+function mouseClicked() {
+    let anySelected = false;
+    if (currentSelected instanceof Wire) currentSelected.selected = false;
+    connectionNodes.forEach((node) => {
+        if (node.didClick()) {
+            if (!currentStartNode) {
+                currentStartNode = node;
+                console.log('current start node: ', currentStartNode);
+                components.push(currentStartNode);
+            }
+            else {
+                components.push(node);
+                console.log(components);
+                wireManager.addWire(components);
+                console.log('adding wire from: ', currentStartNode, ' to ', node);
+                currentStartNode = null;
+                components = [];
+            }
+            anySelected = true;
+        }
+    });
+    wireManager.wires.forEach((wire) => {
+        if (wire.didClick()) {
+            console.log('clicked on wire ', wire);
+            currentSelected = wire;
+            wire.selected = true;
+            anySelected = true;
+        }
+    })
+    if (!anySelected && currentStartNode) {
+        const v = createVector(mouseX, mouseY)
+        // line(currentStartNode.x, currentStartNode.y, v.x, v.y);
+        components.push(v);
+        currentStartNode = v;
+    }
+
+    if (!anySelected) { currentSelected = null; console.log('setting curretnSelcted to ', currentSelected); }
+}
+
 export function draw() {
     clear();
 
@@ -186,6 +248,14 @@ export function draw() {
         const highlight = val.mouseOver() && !currentModal;
         val.draw(highlight);
     });
+
+    wireManager.draw();
+
+    if (components)
+        new Wire(components).draw();
+
+    if (currentStartNode)
+        line(currentStartNode.x, currentStartNode.y, mouseX, mouseY);
 }
 
 export function setup() {
@@ -195,7 +265,10 @@ export function setup() {
     setup_demodulation();
 }
 
+/** @type {Window} */
 window.setup = setup;
 window.draw = draw;
 window.windowResized = windowResized;
+window.onclick = mouseClicked;
 window.doubleClicked = doubleClicked;
+window.onkeydown = keyPressed;
