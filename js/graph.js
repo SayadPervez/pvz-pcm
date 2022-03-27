283648f1c3e3ae97b2e02ac38c27c220df0c7f54
Sayad Pervez <pervez2504@gmail.com>
2022-03-27 23:37:15 +0530

    Evaluate block output paramteres integrated

--- js/graph.js
+++ js/graph.js
@@ -218,6 +218,10 @@ function draw() {
     vertical_scaling_factor = vertical_scale_element.value;
     horizontal_scaling_factor = horizontal_scale_element.value;
     bit_length_element.innerText = bl_scale_element.value;
+    var nw = bl_scale_element.value*20;
+    document.getElementById("transmissionbandwidth").innerHTML=nw;
+    document.getElementById("signalingrate").innerHTML=nw*2;
+    document.getElementById("SQNR_id").innerHTML=(bl_scale_element.value*6)+1.8;
 
     drawGraph();
 
